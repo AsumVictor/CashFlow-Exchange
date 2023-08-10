8c14f63e3ad7fa8e199f6324d87e55c4f4e7f6fc
AsumVictor <Iamasum369@gmail.com>
2023-08-10 18:58:08 +0000

    Added footer

--- src/layout/PageLayout.js
+++ src/layout/PageLayout.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function PageLayout() {
   return (
     <div className="w-full h-screen overflow-y-auto overflow-x-hidden">
       <Header />
       <Outlet />
+      <Footer /> 
     </div>
   );
 }
