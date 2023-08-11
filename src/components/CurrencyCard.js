4bd1104973e2c6f5f7bec22f982e7316baa11382
AsumVictor <Iamasum369@gmail.com>
2023-08-11 09:17:07 +0000

    Refactor

--- src/components/CurrencyCard.js
+++ src/components/CurrencyCard.js
@@ -3,14 +3,18 @@ import { Link } from "react-router-dom";
 import { BsArrowRightCircle } from "react-icons/bs";
 import PropTypes from "prop-types";
 function CurrencyCard({ currencyName, currencySymbol }) {
-  
   return (
-    <Link to={`/${currencySymbol}`} className="currency-card relative w-full flex justify-center items-center px-2 py-2 h-[6cm]">
+    <Link
+      to={`/${currencySymbol}`}
+      className="currency-card relative w-full flex justify-center items-center px-2 py-2 h-[6cm]"
+    >
       <button className="absolute right-4 top-2">
         <BsArrowRightCircle color="white" size={30} />
       </button>
       <h3 className="absolute right-4 bottom-2 text-white text-right">
-        <p className="text-xl 400px:px-2 500px:text-2xl font-semibold">{currencyName}</p>
+        <p className="text-xl 400px:px-2 500px:text-2xl font-semibold">
+          {currencyName}
+        </p>
         <p>{currencyName?.slice(0, 2)}</p>
       </h3>
       <div>
